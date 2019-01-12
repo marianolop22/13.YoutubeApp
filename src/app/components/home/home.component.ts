fdf82a5eda5230f7e59eab295b2a2d514daecaf6
Mariano Lopez <marianolop22@yahoo.com.ar>
2019-01-12 03:27:58 -0300

    fin de ejecrcicio

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { YoutubeService } from 'src/app/services/youtube.service';
 
+declare var $:any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public videos:any[]= [];
+  public videoSel: any = "";
+
+  constructor( private _youtube: YoutubeService )  { 
+
+    this._youtube.getVideos ().subscribe ( 
+      response => {
+        this.videos = response;
+    }, reject => {
+        console.log ( reject );
+
+    })
+  }
 
   ngOnInit() {
   }
+
+  public verVideo ( video: string) {
+    this.videoSel = video;
+    $('#myModal').modal();
+  }
+
+  public cerrarModal () {
+    this.videoSel = null;
+    $('#myModal').modal('hide');
+  }
+
+  public cargar () {
+
+    this._youtube.getVideos ().subscribe ( 
+      response => {
+        this.videos.push.apply (this.videos, response );
+        console.log (this.videos)
+    }, reject => {
+        console.log ( reject );
+
+    })
+  }
+
+
 
 }
--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
 
-  constructor() { }
+  // https://www.googleapis.com/youtube/v3/playlistItems
+  url: string = "https://www.googleapis.com/youtube/v3";
+  apiKey: string = "AIzaSyAUf08WwgF2RGnYNVsezd7By9GPAJELg_s";
+  nextPage:string = "";
+
+
+  constructor( private http:HttpClient ) { }
+
+  public getVideos (): Observable<any> {
+
+    let url = `${this.url}/playlistItems`;
+
+    let params:any = {
+      'part': 'snippet',
+      'maxResults': '10',
+      'playlistId': 'UUqwMD9d8T4TNeN5C6LZfCTw',
+      'key': this.apiKey
+
+    }
+    
+    if ( this.nextPage ) {
+      params.pageToken = this.nextPage ;
+    }
+
+    return this.http.get ( url, {params}  ).pipe(
+      map ( (response:any)=>{
+        this.nextPage = response.nextPageToken;
+        
+        let videos: any[] = [];
+        for ( let video of response.items ) {
+
+          videos.push ( video.snippet);
+          
+        }
+
+        return videos;
+
+      }) 
+
+
+
+    );
+
+
+  }
+
+
 }
